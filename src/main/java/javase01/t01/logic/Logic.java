3f39c899039225a868328512fa6c2b473bd16d83
Vyacheslav Lapin <Vyacheslav_Lapin@epam.com>
2015-10-25 16:41:33 +0300

    Task 1.1

---
+++ src/main/java/javase01/t01/logic/Logic.java
@@ -0,0 +1,12 @@
+package javase01.t01.logic;
+
+/**
+ * Logic.
+ *
+ * @author Vyacheslav_Lapin
+ */
+public class Logic {
+    public String method(){
+        return "I am string in Logic.";
+    }
+}
---
+++ src/main/java/javase01/t01/main/Main.java
@@ -0,0 +1,17 @@
+package javase01.t01.main;
+
+import javase01.t01.logic.Logic;
+
+/**
+ * Main.
+ *
+ * @author Vyacheslav_Lapin
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(new Logic().method());
+
+    }
+}
