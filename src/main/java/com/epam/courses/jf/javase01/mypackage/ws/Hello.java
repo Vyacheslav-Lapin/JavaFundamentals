1529ef7dd7b2916bf3b2fc852b90e8669025cd98
Vyacheslav Lapin <Vyacheslav_Lapin@epam.com>
2016-03-23 22:41:11 +0300

    WS

---
+++ src/main/java/com/epam/courses/jf/javase01/mypackage/ws/Hello.java
@@ -0,0 +1,13 @@
+package com.epam.courses.jf.javase01.mypackage.ws;
+
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import static javax.jws.soap.SOAPBinding.Style.RPC;
+
+@WebService
+@SOAPBinding(style= RPC)
+public class Hello {
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+}
